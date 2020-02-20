783dd6f692ccdbdcd973074e2053011f5356baeb
Tomonobu Saito <Tomnobu.Saito@gmail.com>
2020-02-20 13:49:46 +0900

    my 2nd modification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hello, world from Azure App Service by Tomonobu on Linux!"
+    render html: "Hello, world from Azure App Service by Tomonobu on Linux!!"
   end
 end
+
